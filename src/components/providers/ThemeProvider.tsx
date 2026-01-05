ef9f3e90e4c1b47ef8b135f93183b7e79d097cec
BloodyRoom <sabadashkamaksym@gmail.com>
2026-01-05 23:06:13 +0200

    theme provider fix

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -45,6 +45,7 @@ export const ThemeProvider = ({ children }: { children: ReactNode }) => {
     );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
     const ctx = useContext(ThemeContext);
     if (!ctx) {
