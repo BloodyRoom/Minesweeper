687ce04ff0f089ce359516ceda0797df0bcf085e
BloodyRoom <sabadashkamaksym@gmail.com>
2026-01-10 21:03:50 +0200

    Add random image on a bg

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@ function App() {
 
     return (
         <>
-            <button onClick={toggleTheme}>
-                {theme === "dark" ? "🌙 Dark" : "☀️ Light"}
-            </button>
+            <div className="w-full h-full flex items-center justify-center">
+                <button onClick={toggleTheme} className={"z-1"}>
+                    {theme === "dark" ? "🌙 Dark" : "☀️ Light"}
+                </button>
+
+                <img className={`absolute top-0 ${theme === "dark" ? "opacity-20" : "opacity-100"} z-0`} src={`https://picsum.photos/${window.innerWidth}/${window.innerHeight}/?blur&grayscale`} alt="backround"/>
+            </div>
         </>
     )
 }
